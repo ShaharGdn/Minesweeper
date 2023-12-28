fea9af9610fe7b5e21a5643a67979b42b8eb25af
ShaharGdn <155108581+ShaharGdn@users.noreply.github.com>
2023-12-28 22:37:36 +0200

    Add files via upload

---
+++ js/util.js
@@ -0,0 +1,73 @@
+'use strict'
+
+function renderBoard(mat, selector) {
+
+    var strHTML = '<table><tbody>'
+    for (var i = 0; i < mat.length; i++) {
+
+        strHTML += '<tr>'
+        for (var j = 0; j < mat[0].length; j++) {
+
+            const cell = mat[i][j]
+            const className = `cell cell-${i}-${j}`
+
+            strHTML += `<td class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
+
+function renderCell(location, value) {
+    const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
+    elCell.innerHTML = value
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+function getClassName(location) {
+    const cellClass = `.cell-${location.i}-${location.j}`
+    return cellClass
+}
+
+function countNegs(board, rowIdx, colIdx) {
+    var count = 0
+    // var negs = []
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= board[0].length) continue
+            var currCell = board[i][j]
+            if (currCell === EMPTY) {
+                //change
+                count++
+            }
+        }
+    }
+    return count
+}
+
+function showElement(selector) {
+    const el = document.querySelector(selector)
+    el.classList.remove('hide')
+}
+
+function hideElement(selector) {
+    const el = document.querySelector(selector)
+    el.classList.add('hide')
+}
